d47773caf016603679b3b4f54bccff0202e131b5
Sunil <adhikarisunil339@gmail.com>
2025-02-27 03:28:45 +0545

    commit control

---
+++ control.js
@@ -0,0 +1,38 @@
+let userAge = 12;
+let isCitizen = true;
+let isRegistered = true;
+
+
+// if (userAge >= 18){
+//     if (isCitizen){
+//         if (isRegistered){
+//             console.log("You are eligible to vote.");
+//         }
+//         else{
+//                 console.log("You are not eligible due to registration status.");
+//         }
+//     }
+//     else{
+//             console.log("You are not eligible due to citizenship status.");
+//         }
+// else{
+//         console.log("You are not eligible to vote.");
+//     }
+// }
+
+
+
+
+if (userAge >= 18) {
+  if (isCitizen) {
+    if (isRegistered) {
+      console.log("You are eligible to vote");
+    } else {
+      console.log("You are not eligible due to registration status");
+    }
+  } else {
+    console.log("you are not eligible due to citizenship status");
+  }
+} else {
+  console.log("You are not eligible to vote (Younger)");
+}
