e26da57719c490adca164508a67ad5a681c3633c
Sunil <adhikarisunil339@gmail.com>
2025-02-27 17:31:43 +0545

    commit switch

--- switch.js
+++ switch.js
@@ -1,24 +1,55 @@
-let day = "Sunday";
+// let day = "Sunday";
 
 
-switch (day){
-   case "Monday":
-    console.log("today is Monday");
-    break;
-
-
-
-    case "Friday":
-     console.log("today is Friday");
-     break;
+// switch (day){
+//    case "Monday":
+//     console.log("today is Monday");
+//     break;
 
 
 
-    case "Monday":
-     console.log("today is Monday");
-     break;
+//     case "Friday":
+//      console.log("today is Friday");
+//      break;
+
+
+
+//     case "Monday":
+//      console.log("today is Monday");
+//      break;
  
 
-    default:
-        console.log("No condition Match");
-}
+//     default:
+//         console.log("No condition Match");
+// }
+
+
+//  var areaOfShapes = "square";
+//  var result;
+
+//  switch(areaOfShapes){
+//     case "square":
+//         var a = 5;
+//         result = a * a;
+//         console.log(result);
+//         break;
+
+
+//     case "rectangle":
+//         var a = 5;
+//         var b = 10;
+//         result = a * b;
+//         console.log(result);
+//         break;
+
+
+//     case "circle":
+//         var r = 14;
+//         result = 3.142 * (r * r);
+//         console.log(result);
+//         break;
+
+
+//     default:
+//         console.log("No match found.");
+//  }
