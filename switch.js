9da9c04e872993a9b34be58bb9f4175764782644
Sunil <adhikarisunil339@gmail.com>
2025-02-27 07:59:03 +0545

    commit switch

---
+++ switch.js
@@ -0,0 +1,24 @@
+let day = "Sunday";
+
+
+switch (day){
+   case "Monday":
+    console.log("today is Monday");
+    break;
+
+
+
+    case "Friday":
+     console.log("today is Friday");
+     break;
+
+
+
+    case "Monday":
+     console.log("today is Monday");
+     break;
+ 
+
+    default:
+        console.log("No condition Match");
+}
