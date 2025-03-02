95d6243408574400e663e8cf85b0ab6ac02f2adf
Sunil <adhikarisunil339@gmail.com>
2025-03-03 05:02:24 +0545

    commit function

---
+++ function.js
@@ -0,0 +1,16 @@
+// function sum(a, b){
+//     return a + b;
+
+// }
+// console.log(sum(5, 15));
+// console.log(sum(5, 25));
+// console.log(sum(5, 355));
+// console.log(sum(5, 45));
+
+
+function greet(name){
+    console.log("Hello " + name + ", WElcome to our class.")
+}
+
+greet("ram");
+greet("shyam")
